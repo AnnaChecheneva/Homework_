aacbaa8d547d59f7dd907330d2f03a0cbb424270
Anyachecheneva07@gmail.com <anyachecheneva07@gmail.com>
2022-10-20 21:38:50 +0200

    hw 6 seminar

---
+++ hwSeminar6/Task01/Program.cs
@@ -0,0 +1,44 @@
+﻿// Задача 1: 
+// Пользователь вводит с клавиатуры M чисел. 
+// Посчитайте, сколько чисел больше 0 ввёл пользователь.
+int[] CreateArray(int M)
+{
+    int[] array = new int[M];
+    Random rnd = new Random();
+    for (int i = 0; i< M; i++)
+    {
+        array[i]=rnd.Next(-100, 100);
+    }
+    return array;
+}
+void PrintArray(int[] array)
+{
+    System.Console.WriteLine("Generate array:");
+    for (int i = 0; i < array.Length; i++)
+    {
+        System.Console.Write($"{array[i]}\t");
+    }
+    System.Console.WriteLine();
+}
+int CountPositive(int[] array, int M)
+{
+    int count = 0;
+    for( int i = 0; i < M; i++)
+    {
+        if (array[i]>=0)
+        {
+            count += 1;
+        }
+    }
+    return count;
+}
+System.Console.WriteLine("Hello, friend! This program check certain amount of a random numbers and count quantity of a positive number. Cool?");
+System.Console.Write(" Enter how many numbers do you want to add ---> ");
+int M = Convert.ToInt32(Console.ReadLine());
+int[] arr = CreateArray(M);
+System.Console.WriteLine();
+PrintArray(arr);
+int res = CountPositive(arr, M);
+System.Console.WriteLine("Counting....");
+System.Console.WriteLine();
+System.Console.WriteLine($" Count positive numbers ---> {res}");
---
+++ hwSeminar6/Task02/Program.cs
@@ -0,0 +1,34 @@
+﻿// Задача 2. 
+// Напишите программу, которая найдёт точку пересечения двух прямых, 
+// заданных уравнениями y = k1 * x + b1, y = k2 * x + b2;
+//  значения b1, k1, b2 и k2 задаются пользователем.
+int Prompt(string message)
+{
+    System.Console.Write(message);
+    int number = Convert.ToInt32(Console.ReadLine());
+    return number;
+}
+
+int[] FindDoteCrossX()
+{
+    int[] DoteCross = new int[2];
+    DoteCross[0]= Convert.ToInt32((b2-b1)/(k1-k2));
+    DoteCross[1]= Convert.ToInt32(k1 * (b2-b1)/(k1-k2) + b1);
+    return DoteCross;
+}
+void PrintArray(int[] array)
+{
+    int i = 0;
+    System.Console.Write($"[{array[i]};{array[i+1]}]");
+    System.Console.WriteLine();
+}
+System.Console.WriteLine("Data for line 1:");
+    int k1 = Prompt("Enter value for k1 --->");
+    int b1 = Prompt("Enter value for b1 --->");
+    System.Console.WriteLine("Data for line 2:");
+    int k2 = Prompt("Enter value for k2 --->");
+    int b2 = Prompt("Enter value for b2 --->");
+int[] DoteCross = FindDoteCrossX();
+System.Console.WriteLine("Counting.....");
+System.Console.WriteLine("Dote coordinate of cross lines:");
+PrintArray(DoteCross);
